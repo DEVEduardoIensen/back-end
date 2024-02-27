9e34ead7df7f98092898851e23c7b75b58713b80
DEVEeduardoiensen <eduardo.iensen.prestes@escola.pr.gov.br>
2024-02-27 10:48:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const texto1 = "olá mundo";
+const texto2 = "bom dia";
+const numero1 = 1;
+const numero = 299;
+const operação = numero1 + numero;
+console.log(operação); 
+
+// ponto flutuante
+const pontof = 3.3;
+const pfsemzero = .5;
+const resultado = pontof/pfsemzero;
+console.log(resultado);
+
+// infinitos
+var a = 10
+var b = 0
+console.log(a/b) 
+
+
+// NaN
+
+const bike = irineu;
+
+console.log(bike * numero); 
+
+
+
+// booleanos
+const numbe1 = 77;
+const numbe2 = 787;
+let cadastroativado;
+
+console.log(numbe1 === numbe2);
+
+
+// null
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
